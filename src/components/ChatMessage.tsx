9e18f4ff75c6771b22a68e8f8cf3a44efb8d43b7
Mahdi taheri <m.taheri123456789@gmail.com>
2024-11-08 22:16:21 +0330

    Add gradient to server message, increase messages font and make them semibold

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -13,11 +13,11 @@ const ChatMessage = ({ message }: ChatMessageProps) => {
   return (
     <div
       className={cn(
-        "inline-block p-3 text-sm rounded-lg",
+        "inline-block p-3 text-md rounded-lg font-semibold",
         "max-w-[75%] break-words",
         isUserMessage
-          ? "self-start bg-gradient-to-tr from-chat-gradient-1 to-chat-gradient-2 text-white shadow-md"
-          : "self-end bg-gray-400 text-gray-800"
+          ? "self-start bg-gradient-to-tr from-chat-message-1 to-chat-message-2 text-white shadow-md"
+          : "self-end bg-gradient-to-tr from-server-message-1 to-server-message-2 text-white"
       )}
     >
       {message.content}
