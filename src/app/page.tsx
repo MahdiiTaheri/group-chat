c4e072bd4d9618958b066c36aeb92ea3ebb6a8e3
Mahdi taheri <m.taheri123456789@gmail.com>
2024-11-09 18:52:41 +0330

    Add SlideIn animation on entire page when components mounting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,20 @@ const ChatHeader = dynamic(() => import("@/components/ChatHeader"));
 const ChatInput = dynamic(() => import("@/components/ChatInput"));
 const ChatScreen = dynamic(() => import("@/components/ChatScreen"));
 
+import * as motion from "framer-motion/client";
+import { slideInProps } from "@/lib/motions";
+
 const ChatPage = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-between bg-gradient-to-r from-gray-100 to-gray-200 p-4">
-      <ChatHeader />
-      <ChatScreen />
-      <ChatInput />
+    <div className="bg-gradient-to-r from-gray-100 to-gray-200">
+      <motion.div
+        className="flex h-screen flex-col items-center justify-between  p-4"
+        {...slideInProps}
+      >
+        <ChatHeader />
+        <ChatScreen />
+        <ChatInput />
+      </motion.div>
     </div>
   );
 };
